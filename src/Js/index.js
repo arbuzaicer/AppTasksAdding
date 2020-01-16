842d11936fc7f5d7b336e14f67a7859b54ebdc09
arbuzaicer <maxpachinov@gmail.com>
2020-01-16 10:15:06 +0200

    Create preloader component. Add preloader to preRender before Post will show. Add button save/delete to post-item.

--- src/Js/index.js
+++ src/Js/index.js
@@ -3,12 +3,14 @@ import {NavigationComponent} from "../components/navigation.component";
 import {PostsComponent} from "../components/posts.component";
 import {CreateComponent} from "../components/create.component";
 import {FavoriteComponent} from "../components/favorite.component";
+import {LoaderComponent} from "../components/loader.component";
 
 const header = new HeaderComponent('header');
+const preloader = new LoaderComponent('loader');
 
 /*Navigation section*/
 const navigation = new NavigationComponent('navigation');
-const posts = new PostsComponent('posts');
+const posts = new PostsComponent('posts', {preloader});
 const create = new CreateComponent('create');
 const favorite = new FavoriteComponent('favorite');
 
--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -3,33 +3,40 @@ import {sendPost} from "../services/api.service";
 import {TransformData} from "../services/transform.data";
 
 export class PostsComponent extends Component {
-    constructor(id) {
+    constructor(id, {preloader}) {
         super(id);
+
+        this.preloader = preloader;
     }
 
     init() {
-
+        this.$el.addEventListener('click', buttonHandler.bind(this))
     }
 
     async onShow() {
+        this.preloader.show();
         const fbData = await sendPost.getPosts();
         const posts = TransformData.BDTransformData(fbData);
         const html = posts.map(item => {
             return renderPost(item)
         });
         this.$el.innerText = '';
+        this.preloader.hide();
         this.$el.insertAdjacentHTML("afterbegin", html.join(' '))
     }
 
 }
 
 function renderPost(post) {
+    let postType = post.type === 'news' ? `<li class="tag tag-blue tag-rounded">Новость</li>`: `<li class="tag tag-rounded">Заметка</li>`;
+    const addToFavoriteButton = `<button class="button-round button-small button-primary" data-id="${post.id}">Сохранить</button>`;
+
     return `
 <div class="panel" id=${post.id}>
       <div class="panel-head">
         <p class="panel-title">${post.title}</p>
         <ul class="tags">
-          <li class="tag tag-blue tag-rounded">${post.type}</li>
+          ${postType}
         </ul>      
         </div>
       <div class="panel-body">
@@ -37,7 +44,14 @@ function renderPost(post) {
       </div>
       <div class="panel-footer w-panel-footer">
         <small>${post.date}</small>
+        ${addToFavoriteButton}
       </div>
-    </div>
-`
+    </div>`
+}
+
+function buttonHandler(event) {
+    if(event.target.dataset.id) {
+        console.log(event.target.dataset.id)
+    }
+
 }
